5d1fc9fa39ceb13fd81c8d8ad8ced8e9555a1e8f
Martin Bluemel <martin.bluemel@web.de>
2020-04-10 00:00:26 +0200

    - small typo :-) - removed unnecessary trailing blanks from some texts

--- src/de/renatekavuza/quotecollection/Themes.java
+++ src/de/renatekavuza/quotecollection/Themes.java
@@ -11,12 +11,12 @@ public class Themes {
 			i += 1;
 		}
 		Quotes grace = new Quotes("faith",
-				"When you seek the grace of god, you can be assured, that the grace of god is also seeking you. ",
+				"When you seek the grace of god, you can be assured, that the grace of god is also seeking you.",
 				"Ramana Maharashi", "Buddhist");
-		Quotes path = new Quotes("acceptance", "whatever arises in body and mind is to be regarded as path. ",
+		Quotes path = new Quotes("acceptance", "whatever arises in body and mind is to be regarded as path.",
 				"Zen wisdom", "Buddhist");
 		Quotes sameeyes = new Quotes("oneness",
-				"The eye through wich I see god is the same eye through wich god sees me. ", "Meister Eckhardt",
+				"The eye through which I see god is the same eye through which god sees me.", "Meister Eckhardt",
 				"Christian");
 	}
 }
