1dbf16675a7ea8537033622a295b2f1de8653ba4
Martin Bluemel <martin.bluemel@web.de>
2020-04-10 00:09:44 +0200

    Fixed lost UTF-8 encoding for Sanskrit charcters

--- src/de/renatekavuza/quotecollection/Arrayfill.java
+++ src/de/renatekavuza/quotecollection/Arrayfill.java
@@ -6,14 +6,14 @@ public class Arrayfill {
 		String[] fullness = new String[5]; // The 5 skandhas
 
 		fullness[0] = "matter, or body (rūpa)";
-		fullness[1] = "sensations, or feelings (vedan�?)"; // positive, negative, neutral
-		fullness[2] = "perceptions of sense objects (Sanskrit: saṃjñ�?; P�?li: saññ�?)";
-		fullness[3] = "mental formations (saṃsk�?ras/sankh�?ras)";
-		fullness[4] = "awareness, or consciousness, of the other three mental aggregates (vijñ�?na/viññ�?ṇa)";
+		fullness[1] = "sensations, or feelings (vedanā)"; // positive, negative, neutral
+		fullness[2] = "perceptions of sense objects (Sanskrit: saṃjñā; Pāli: saññā)";
+		fullness[3] = "mental formations (saṃskāras/sankhāras)";
+		fullness[4] = "awareness, or consciousness, of the other three mental aggregates (vijñāna/viññāṇa)";
 
 		System.out.println("The 5 Skanthas are: ");
 		for (int i = 0; i < fullness.length; i++) {
-			System.out.println(fullness[i]);
+			System.out.println(String.format("%d: %s", i + 1, fullness[i]));
 		}
 	}
 }
